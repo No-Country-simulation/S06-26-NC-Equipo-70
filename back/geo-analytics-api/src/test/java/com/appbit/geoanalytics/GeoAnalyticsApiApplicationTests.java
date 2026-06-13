513855f20b61472579d6b1c8abe78c02ea5956bd
fr4ncisx <fr4ncisx@gmail.com>
2026-06-13 00:08:04 -0300

    feat: initialize Spring Boot project with Maven
    - Added basic project structure for "geo-analytics-api".
    - Configured Maven Wrapper, `pom.xml`, and essential dependencies.
    - Included application configuration files for `dev` and `prod` environments.
    - Added Spring Boot Main and Test classes.
    - Set up Flyway for database migration management.

---
+++ back/geo-analytics-api/src/test/java/com/appbit/geoanalytics/GeoAnalyticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appbit.geoanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoAnalyticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
