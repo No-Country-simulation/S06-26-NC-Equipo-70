7565e0dc42c0e1ff215037b7e7b0c77b52ea1e6d
fr4ncisx <fr4ncisx@gmail.com>
2026-06-24 04:48:44 -0300

    feat: add base `DomainException` and `IdentityRestrictionException` for geoanalytics domain
    - Introduced abstract `DomainException` as a base class for domain-specific exceptions.
    - Added `IdentityRestrictionException` extending `DomainException` to handle identity-related restrictions.

---
+++ back/src/main/java/com/appbit/geoanalytics/domain/exception/IdentityRestrictionException.java
@@ -0,0 +1,7 @@
+package com.appbit.geoanalytics.domain.exception;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class IdentityRestrictionException extends DomainException {
+}
